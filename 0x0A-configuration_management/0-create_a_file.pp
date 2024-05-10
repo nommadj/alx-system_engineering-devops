df0f21ff6de9a1a32074d4f0ea85517f3d05eff9
Antony Wainaina <mainamasha254@gmail.com>
2024-05-10 13:55:42 +0300

    commit 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# This manifest creates a file in /tmp folder
-
+# Creates a file
 file { '/tmp/school':
-  content =>'I love Puppet',
+  ensure  => present,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,12 +1,4 @@
-# Define an exec resource to kill the process named "killmenow"
-
-
-exec { 'kill_killmenow_process':
-  # Specify the command to kill the process using pkill
-  command => '/usr/bin/pkill killmenow',
-  # Only run the command if the process exists
-  onlyif  => '/usr/bin/pgrep killmenow',
-  # Run the command as root
-  user    => 'root',
-  # Notify if the command changes
+# Executes a command
+exec { 'pkill killmenow':
+  path => '/usr/bin:/usr/sbin:/bin'
 }
